159f6844db18c455ea3472be1cdc73ae6c241a0d
TigaJi <dailinj@outlook.com>
2023-04-21 14:06:09 -0400

    mentornet

--- mentornet/mentornet.py
+++ mentornet/mentornet.py
@@ -1,7 +1,3 @@
-'''
-Implement mentornet_nn using pytorch
-'''
-
 import torch
 import torch.nn as nn
 import numpy as np
@@ -24,6 +20,8 @@ class MentorNet(nn.Module):
         self.fc_1 = nn.Linear(label_embedding_size + epoch_embedding_size + 2, num_fc_nodes)
         self.fc_2 = nn.Linear(num_fc_nodes, 1)
         self.activation = nn.Tanh()
+
+        self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
         
     def forward(self, input_features):
         batch_size = input_features.size(0)
@@ -35,8 +33,8 @@ class MentorNet(nn.Module):
         labels = input_features[:, 2].view(-1, 1).long()
 
         epochs = input_features[:, 3].view(-1, 1).long()
-
-        epochs = torch.min(epochs, torch.ones([batch_size, 1], dtype=torch.int32) * 99)
+        #print(epochs.device)
+        #epochs = torch.min(epochs, torch.ones([batch_size, 1], dtype=torch.int32).to(device) * 99)
 
         if losses.dim() <= 1:
             num_steps = 1
@@ -44,17 +42,26 @@ class MentorNet(nn.Module):
             num_steps = losses.size(1)
 
         lstm_inputs = torch.stack([losses, loss_diffs], dim=2)
-        lstm_inputs = lstm_inputs.squeeze(1)
 
-        out, _ = self.forward_cell(lstm_inputs)
-        out = out.view(batch_size, -1)
+        #lstm_inputs = lstm_inputs.squeeze(1)
+        
+
+        _, (h_n, c_n) = self.forward_cell(lstm_inputs)
+        h_forward = h_n[0,:,:] 
+        h_backward = h_n[1,:,:] 
+        # print(h_forward.shape)
+        # print(h_backward.shape)
+        out = torch.concat((h_forward,h_backward),1)
+        # print(out.shape)
+
+
         
         label_inputs = self.label_embedding[labels.squeeze()]
         epoch_inputs = self.epoch_embedding[epochs.squeeze()]
-        
 
         feat = torch.cat([label_inputs, epoch_inputs, out], dim=1)
-        print(feat)
+
+
         fc_1 = self.activation(self.fc_1(feat))
         out_layer = self.fc_2(fc_1)
 
